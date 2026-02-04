0d9c0469e18dec218b5bcce59467f5e0d3e3a3df
Richard <richard.mace@gmail.com>
2026-02-04 23:33:13 +0000

    Updated json

--- mix.exs
+++ mix.exs
@@ -60,7 +60,7 @@ defmodule Tunebox.MixProject do
       {:telemetry_metrics, "~> 1.0"},
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.26"},
-      {:jason, "~> 1.2"},
+      {:jason, "~> 1.4"},
       {:dns_cluster, "~> 0.2.0"},
       {:bandit, "~> 1.5"}
     ]
