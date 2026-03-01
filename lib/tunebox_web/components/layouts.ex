15ce077ee8438f662f4ebe53cb1c771c89a69ab6
Richard <richard.mace@gmail.com>
2026-03-01 08:10:45 +0000

    Removed redundant website

--- lib/tunebox_web/components/layouts.ex
+++ lib/tunebox_web/components/layouts.ex
@@ -45,9 +45,6 @@ defmodule TuneboxWeb.Layouts do
       <div class="flex-none">
         <ul class="flex flex-column px-1 space-x-4 items-center">
           <li>
-            <a href="https://phoenixframework.org/" class="btn btn-ghost">Website</a>
-          </li>
-          <li>
             <a href="https://github.com/phoenixframework/phoenix" class="btn btn-ghost">GitHub</a>
           </li>
           <li>
